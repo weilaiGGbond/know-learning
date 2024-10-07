83660910cb990a900777464effed9611b1a64d88
CraneReturn <1066063143@qq.com>
2024-10-07 11:32:45 +0800

    聊天

---
+++ src/renderer/components/message/people/mianPeople.tsx
@@ -0,0 +1,54 @@
+//右边弹出框
+import React, { useState } from 'react';
+import { Drawer, Button, Radio, DrawerProps, Space, Avatar, Descriptions, RadioChangeEvent } from 'antd';
+import { EllipsisOutlined } from '@ant-design/icons';
+import '@renderer/assets/styles/message/index.scss'
+import AddNew from './addNew';
+import GroupList from '../group/groupList';
+export default function MianPeople({ contact }) {
+    const [open, setOpen] = useState(false);
+    const [placement] = useState<DrawerProps['placement']>('right')
+
+    const showDrawer = () => {
+        setOpen(true);
+    };
+
+    const onClose = () => {
+        setOpen(false);
+    };
+    const [size, setSize] = useState<'default' | 'middle' | 'small'>('default');
+
+    const onChange = (e: RadioChangeEvent) => {
+        console.log('size checked', e.target.value);
+        setSize(e.target.value);
+    };
+
+    return (
+        <div>
+            <Button icon={<EllipsisOutlined />} shape="circle" onClick={showDrawer} />
+            <Drawer
+                title={
+                    <Space>
+                        <Avatar src={contact.avatar} alt={contact.name}>
+                            {contact.name[0]}
+                        </Avatar>
+                        <p>
+                            {contact.name}
+                        </p>
+                        <AddNew />
+                    </Space>
+                }
+                placement={placement}
+                closable={false}
+                onClose={onClose}
+                open={open}
+                key={placement}
+                className='rightDraw'
+            >
+                <div>
+                  <GroupList/>
+                </div>
+            </Drawer>
+        </div>
+    )
+}
--- src/renderer/router/index.tsx
+++ src/renderer/router/index.tsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
     element: <MemorandumIndex />
   },
   {
-    
+
     path: '/message',
     element: <Message />,
     children: [
